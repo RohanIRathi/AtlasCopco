63655741386a7ca133e5669b6162061b9c3d477c
Rohan Rathi <rathirohan8@gmail.com>
2020-11-05 00:36:26 +0530

    Converted spaces to tabs

--- atlas_project/settings.py
+++ atlas_project/settings.py
@@ -30,17 +30,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 local_apps = [
-    'entry.apps.EntryConfig',
-    'home.apps.HomeConfig',
+	'entry.apps.EntryConfig',
+	'home.apps.HomeConfig',
 ]
 
 default_apps = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 ]
 
 third_party_apps = ['crispy_forms', ]
@@ -48,31 +48,31 @@ third_party_apps = ['crispy_forms', ]
 INSTALLED_APPS = local_apps + default_apps + third_party_apps
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'atlas_project.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'atlas_project.wsgi.application'
@@ -81,28 +81,28 @@ WSGI_APPLICATION = 'atlas_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
+	}
 }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 # Internationalization
--- home/views.py
+++ home/views.py
@@ -16,140 +16,140 @@ from .forms import *
 from entry.models import *
 
 def is_admin(user):
-    try:
-        admin = Employee.objects.get(user=user.id).admin
-        return admin
-    except:
-        return False
+	try:
+		admin = Employee.objects.get(user=user.id).admin
+		return admin
+	except:
+		return False
 
 @login_required
 @user_passes_test(is_admin)
 def signup(request):
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        employee_form = CreateEmployeeForm(request.POST)
-        if form.is_valid() and employee_form.is_valid():
-            user = form.save()
-            employee = employee_form.save(commit=False)
-            employee.user = user
-            employee.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created ')
-            return redirect('/login/')
-    form = CreateUserForm()
-    employee_form = CreateEmployeeForm()
-    context = {'form': form, 'employee_form': employee_form}
-    return render(request, 'registration/signup.html', context)
+	if request.method == "POST":
+		form = CreateUserForm(request.POST)
+		employee_form = CreateEmployeeForm(request.POST)
+		if form.is_valid() and employee_form.is_valid():
+			user = form.save()
+			employee = employee_form.save(commit=False)
+			employee.user = user
+			employee.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created ')
+			return redirect('/login/')
+	form = CreateUserForm()
+	employee_form = CreateEmployeeForm()
+	context = {'form': form, 'employee_form': employee_form}
+	return render(request, 'registration/signup.html', context)
 
 
 def login_validate(request):
-    if request.method == "POST":
-        user_name = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=user_name, password=password)
-        if user is not None:
-            print('User')
-            login(request, user)
-            return redirect(request.GET.get('next', 'home'))
-        else:
-            print('Not a User')
-            messages.info(request, "Email or Password incorrect !")
+	if request.method == "POST":
+		user_name = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=user_name, password=password)
+		if user is not None:
+			print('User')
+			login(request, user)
+			return redirect(request.GET.get('next', 'home'))
+		else:
+			print('Not a User')
+			messages.info(request, "Email or Password incorrect !")
 
-    return render(request, 'registration/login.html')
+	return render(request, 'registration/login.html')
 
 @login_required
 def logout_user(request):
-    logout(request)
-    return redirect('/')
+	logout(request)
+	return redirect('/')
 
 
 class VisitorListView(LoginRequiredMixin, ListView):
-    def get(self, request):
-        visitor_list = Visitor.objects.filter(out_time__isnull=True)
-        print(visitor_list)
-        context = {'visitor_list': visitor_list}
+	def get(self, request):
+		visitor_list = Visitor.objects.filter(out_time__isnull=True)
+		print(visitor_list)
+		context = {'visitor_list': visitor_list}
 
-        return render(request, 'home/home.html', context)
+		return render(request, 'home/home.html', context)
 
 class NotVisitedListView(LoginRequiredMixin, ListView):
-    def get(self, request):
-        visitor_list = Visitor.objects.filter(in_time__isnull=True)
-        context = {'visitor_list': visitor_list}
+	def get(self, request):
+		visitor_list = Visitor.objects.filter(in_time__isnull=True)
+		context = {'visitor_list': visitor_list}
 
-        return render(request, 'home/not_visited.html', context)
+		return render(request, 'home/not_visited.html', context)
 
 class AllVisitedListView(LoginRequiredMixin, ListView):
-    def get(self, request):
-        visitor_list = Visitor.objects.filter(out_time__isnull = False).order_by('-in_time')
-        context = {'visitor_list': visitor_list}
+	def get(self, request):
+		visitor_list = Visitor.objects.filter(out_time__isnull = False).order_by('-in_time')
+		context = {'visitor_list': visitor_list}
 
-        return render(request, 'home/all_visitors.html', context)
+		return render(request, 'home/all_visitors.html', context)
 
 class VisitorDetailView(LoginRequiredMixin, DetailView):
-    model = Visitor
-    template_name = 'home/visitor_view.html'
+	model = Visitor
+	template_name = 'home/visitor_view.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if context['visitor'].photo_id:
-            context['photopath'] = os.path.relpath(str(context['visitor'].photo_id))
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		if context['visitor'].photo_id:
+			context['photopath'] = os.path.relpath(str(context['visitor'].photo_id))
 
-        return context
+		return context
 
 @login_required
 def photoscan(request, **kwargs):
-    instance = get_object_or_404(Visitor, pk = kwargs.get('id'))
-    form = PhotoForm()
-    if request.method == 'POST':
-        form = PhotoForm(request.POST, request.FILES, instance=instance)
-        if form.is_valid():
-            form.save()
-            success_url = reverse('entry:scanQR', kwargs={'id': kwargs.get('id')})
-            return redirect(success_url)
-    return  render(request, 'home/photoscan.html', {'form':form})
+	instance = get_object_or_404(Visitor, pk = kwargs.get('id'))
+	form = PhotoForm()
+	if request.method == 'POST':
+		form = PhotoForm(request.POST, request.FILES, instance=instance)
+		if form.is_valid():
+			form.save()
+			success_url = reverse('entry:scanQR', kwargs={'id': kwargs.get('id')})
+			return redirect(success_url)
+	return  render(request, 'home/photoscan.html', {'form':form})
 
 class AllVisitorsListView(LoginRequiredMixin, ListView):
-    def get(self, request):
-        visitor_list = Visitor.objects.order_by('-in_time')
-        context = {'visitor_list': visitor_list}
+	def get(self, request):
+		visitor_list = Visitor.objects.order_by('-in_time')
+		context = {'visitor_list': visitor_list}
 
-        return render(request, 'home/all_visitors_booked.html', context)
-    
+		return render(request, 'home/all_visitors_booked.html', context)
+	
 class AllUsersListView(LoginRequiredMixin, ListView):
-    def get(self, request):
-        employee_list = Employee.objects.all()
-        context = {'employee_list': employee_list}
-        
-        return render(request, 'home/all_users_list.html', context)
-    
+	def get(self, request):
+		employee_list = Employee.objects.all()
+		context = {'employee_list': employee_list}
+		
+		return render(request, 'home/all_users_list.html', context)
+	
 @login_required()
 @user_passes_test(is_admin)
 def get_table_data(request):
-    visitor_list = Visitor.objects.order_by('-in_time')
-    search_query = ''
-    if request.method == 'POST':
-        search_query = request.POST['search']
-        for i in [1]:
-            print(visitor_list[1].in_time.date(), search_query)
-            search_date = None
-            try:
-                search_date = datetime.strptime(search_query, '%d-%m-%Y')
-                print(search_date)
-            except:
-                pass
-            try:
-                user = Employee.objects.get(user=User.objects.get(username=search_query))
-                visitor_list = Visitor.objects.filter(user=user)
-                break
-            except:
-                pass
-            if search_date:
-                visitor_list = Visitor.objects.filter(in_time__date = search_date)
-            else:
-                visitor_list = Visitor.objects.filter(name=search_query)
-        if search_query == '':
-            visitor_list = Visitor.objects.order_by('-in_time')
+	visitor_list = Visitor.objects.order_by('-in_time')
+	search_query = ''
+	if request.method == 'POST':
+		search_query = request.POST['search']
+		for i in [1]:
+			print(visitor_list[1].in_time.date(), search_query)
+			search_date = None
+			try:
+				search_date = datetime.strptime(search_query, '%d-%m-%Y')
+				print(search_date)
+			except:
+				pass
+			try:
+				user = Employee.objects.get(user=User.objects.get(username=search_query))
+				visitor_list = Visitor.objects.filter(user=user)
+				break
+			except:
+				pass
+			if search_date:
+				visitor_list = Visitor.objects.filter(in_time__date = search_date)
+			else:
+				visitor_list = Visitor.objects.filter(name=search_query)
+		if search_query == '':
+			visitor_list = Visitor.objects.order_by('-in_time')
 
-    context = {'visitor_list': visitor_list, 'search_query': search_query}
-    
-    return render(request, 'home/table.html', context=context)
+	context = {'visitor_list': visitor_list, 'search_query': search_query}
+	
+	return render(request, 'home/table.html', context=context)
