cbc3d0b98e094f29974143369d5e6646d850269b
Yash <yash.21810545@viit.ac.in>
2021-01-29 19:47:33 +0530

    Changes in charts

--- charts/views.py
+++ charts/views.py
@@ -6,75 +6,74 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from entry.models import Visitor
 from home.views import is_admin
+
+
 @login_required
 def home1(request):
     return render(request, 'charts/chart1.html')
 
+
 @login_required
 def home2(request):
     return render(request, 'charts/chart2.html')
+
 
 @login_required
 def home3(request):
     return render(request, 'charts/chart3.html')
 
 
-
 @login_required
 @user_passes_test(is_admin)
 def visitor_chart1(request):
-    labels = ['']
-    data = [0]
+    labels = []
+    data = []
     for i in range(7):
         d = datetime.today() - timedelta(days=i)
         labels.append(d.strftime("%d-%m-%Y"))
         data.append(Visitor.objects.filter(in_time__date=d.date()).count())
-    labels.append('')
-    data.append(0)
-    print(labels,data)
+
     return JsonResponse(data={
         'labels': labels,
         'data': data,
         'scaletype': 'Days',
     })
 
+
 @login_required
 @user_passes_test(is_admin)
 def visitor_chart2(request):
-    labels = ['']
-    data = [0]
-    '''for i in range(30):
-        d = datetime.today() - timedelta(months=i)
-        labels.append(d.strftime("%d-%m-%Y"))
-        data.append(Visitor.objects.filter(in_time__date=d.date()).count())
-    print(labels)
-    print(data)'''
-    result = Visitor.objects.annotate(month=TruncMonth('in_time')).values('month').annotate(c=Count('id')).values('month', 'c')
+    labels = []
+    data = []
+
+    result = reversed(
+        Visitor.objects.annotate(month=TruncMonth('in_time')).values('month').annotate(c=Count('id')).values('month',
+                                                                                                             'c'))
 
     for i in result:
+        if len(labels) == 13:
+            break
         try:
             labels.append(i['month'].strftime("%m-%Y"))
             data.append(i['c'])
         except:
             pass
-    labels.append('')
-    data.append(0)
+
     return JsonResponse(data={
         'labels': labels,
         'data': data,
         'scaletype': 'Months',
     })
 
+
 @login_required
 @user_passes_test(is_admin)
 def visitor_chart3(request):
-    labels = ['']
-    data = [0]
-    """for i in range(12):
-        d = datetime.today() - timedelta(days=i)
-        labels.append(d.strftime("%d-%m-%Y"))
-        data.append(Visitor.objects.filter(in_time__date=d.date()).count())"""
-    result = Visitor.objects.annotate(year=TruncYear('in_time')).values('year').annotate(c=Count('id')).values('year', 'c')
+    labels = []
+    data = []
+
+    result = reversed(
+        Visitor.objects.annotate(year=TruncYear('in_time')).values('year').annotate(c=Count('id')).values('year', 'c'))
 
     for i in result:
         try:
@@ -82,10 +81,9 @@ def visitor_chart3(request):
             data.append(i['c'])
         except:
             pass
-    labels.append('')
-    data.append(0)
+
     return JsonResponse(data={
         'labels': labels,
         'data': data,
-        'scaletype':'Years',
+        'scaletype': 'Years',
     })
